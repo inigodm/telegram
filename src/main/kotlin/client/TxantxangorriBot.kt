fec76a8bf9824e88061a1f25e72d3b5c01ef1285
inigo delgado <i.delgado@lookiero.com>
2020-10-26 21:45:15 +0100

    Modified main

--- src/main/kotlin/client/TxantxangorriBot.kt
+++ src/main/kotlin/client/TxantxangorriBot.kt
@@ -10,11 +10,9 @@ import org.telegram.telegrambots.meta.api.objects.Update
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException
 
 
-class TxantxangorriBot: TelegramLongPollingBot() {
+class TxantxangorriBot(val brain: Brain = Brain(Client()), val chatsIds: MutableList<Long> = mutableListOf()): TelegramLongPollingBot() {
     val TOKEN = "1309064279:AAFLFm6TrWeWUk_A510WSDj9pOiRiAghD28"
     val NAME = "Txantxangorri_bot"
-    val CHAT_IDS = mutableSetOf<Long>()
-    val brain = Brain(Client())
     /**
      * Returns the token of the bot to be able to perform Telegram Api Requests
      * @return Token of the bot
@@ -30,8 +28,12 @@ class TxantxangorriBot: TelegramLongPollingBot() {
     override fun onUpdateReceived(update: Update?) {
         val messageTextReceived = update!!.message.text
         val chatId = update.message.chatId
-        CHAT_IDS.add(chatId)
+        chatsIds.add(chatId)
         println(chatId)
+        answerMessage(messageTextReceived, chatId)
+    }
+
+    public fun answerMessage(messageTextReceived: String, chatId: Long) {
         var accum = ""
         var i = 0
         brain.answer(messageTextReceived).forEach {

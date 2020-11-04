ee60fa0442004f36732b15db474946274a5a4714
inigo delgado <i.delgado@lookiero.com>
2020-11-04 23:08:33 +0100

    Remove password

---
+++ src/main/kotlin/inigo/client/TxantxangorriBot.kt
@@ -0,0 +1,78 @@
+package inigo.client
+
+import inigo.client.api.Client
+import inigo.config.PropertiesReader
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import org.telegram.telegrambots.meta.api.methods.ParseMode
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.objects.Update
+
+
+class TxantxangorriBot(val brain: Brain = Brain(Client()),
+                       val chatsIds: MutableList<Long> = mutableListOf(),
+                       val logger: Logger = LoggerFactory.getLogger(TxantxangorriBot::javaClass.name)): TelegramLongPollingBot() {
+    val TOKEN = PropertiesReader.getProperties().getProperty("token")
+    val NAME = PropertiesReader.getProperties().getProperty("name")
+    /**
+     * Returns the token of the bot to be able to perform Telegram Api Requests
+     * @return Token of the bot
+     */
+    override fun getBotToken(): String {
+        return TOKEN
+    }
+
+    /**
+     * This method is called when receiving updates via GetUpdates method
+     * @param update Update received
+     */
+    override fun onUpdateReceived(update: Update?) {
+        logger.info("Received : $update")
+        val messageTextReceived = update!!.message.text
+        val chatId = update.message.chatId
+        chatsIds.add(chatId)
+        logger.info("Received a message from $chatId")
+        answerMessage(messageTextReceived, chatId)
+        logger.info("Finished $chatId's $update")
+    }
+
+    fun answerMessage(messageTextReceived: String, chatId: Long) {
+        var accum = ""
+        var i = 0
+        brain.answer(messageTextReceived).forEach {
+            if (i < 20) {
+                i++
+                accum += it
+            } else {
+                sendMessage(accum, chatId)
+                accum = ""
+                i = 0
+            }
+        }
+        if (accum.isNotEmpty()) {
+            sendMessage(accum, chatId)
+        }
+    }
+
+    /**
+     * Return bot username of this bot
+     */
+    override fun getBotUsername(): String {
+        return  NAME
+    }
+
+    fun sendMessage(message: String, chatId: Long) {
+        val sendMessage = SendMessage()
+                .setChatId(chatId)
+                .setParseMode(ParseMode.HTML)
+                .setText(message)
+        logger.info("Mandando mensaje: $message a $chatId")
+        try{
+            execute(sendMessage)
+        } catch (e: Exception){
+            e.printStackTrace()
+        }
+        logger.info("Mandado mensaje: $message a $chatId")
+    }
+}
